d4aa0490209c2705796b2036789f9178aef1235e
Taher <taherabuzeid68@gmail.com>
2025-04-21 19:25:08 +0200

    creat Taher branch

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,8 @@ import { provideStorage, getStorage } from '@angular/fire/storage';
 import { environment } from '../environments/environment';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+// import Aura from '@primeng/themes/aura';
+import Material from '@primeng/themes/material';
 
 
 import { routes } from './app.routes';
@@ -16,7 +17,9 @@ export const appConfig: ApplicationConfig = {
     provideFirestore(() => getFirestore()), provideAnimationsAsync(),provideStorage(() => getStorage()),
         providePrimeNG({
             theme: {
-                preset: Aura
+                // preset: Aura
+                preset: Material
             }
-        })]
+    })
+  ]
 };
