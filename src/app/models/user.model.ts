2287d2cd5ebcf86d2d8b2850cd0d9ef823162ecd
Taher <taherabuzeid68@gmail.com>
2025-04-21 04:26:16 +0200

    Moved user.model.ts to new folder

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,17 @@
+
+export interface User {
+    userId: string;
+    fullName: string;
+    email: string;
+    phone: string;
+    role: 'customer' | 'main admin' | 'shop manager' | 'vendor';
+    address: string[];
+    wishlist: {
+      productId: string;
+      title: { en: string; ar: string };
+      mainImage: string;
+      price: number;
+    }[];
+    createdAt: Date;
+    updatedAt: Date;
+  }
